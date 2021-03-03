cd6fd2deb5a177565edcfeffe7ac9e047edffb44
EMRE ÇELİK <celikemre1996@hotmail.com>
2021-03-03 19:39:25 +0300

    Adres class eklendi

--- src/Adres.java
+++ src/Adres.java
@@ -1,3 +1,13 @@
 public class Adres {
+    public int capacity;
+    public int tutulan_vib;
+    public int x_cord;
+    public int y_cord;
+
+    Adres(int c,int x,int y){
+        capacity=c;
+        x_cord=x;
+        y_cord=y;
+    }
 
 }
